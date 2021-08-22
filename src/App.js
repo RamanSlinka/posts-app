19b081e106e989de0b74ef19f31b908131da984a
RamanSlinka <romuch1986@gmail.com>
2021-08-22 13:53:59 +0300

    use pagination on page and add component Pagination

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount, getPagesArray} from "./utils/pages";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
 
@@ -19,14 +21,21 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    const [totalPages, setTotalPages] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [page, setPage] = useState(1);
+
+
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
-    })
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit));
+    });
 
     useEffect(() => {
         fetchPosts()
-    }, [])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -38,6 +47,10 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const changePage = (page) => {
+        setPage(page)
+
+    }
 
     return (
         <div className="App">
@@ -67,8 +80,10 @@ function App() {
                     remove={removePost}
                     posts={sortedAndSearchedPosts} title="List of posts"/>
             }
-
-
+            <Pagination changePage={changePage}
+                        page={page}
+                        totalPages={totalPages}
+            />
         </div>
     );
 }
