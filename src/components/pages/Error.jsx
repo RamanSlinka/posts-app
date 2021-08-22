dc9ec721c148854d862267d43436b724e2c2fc9b
RamanSlinka <romuch1986@gmail.com>
2021-08-22 14:59:29 +0300

    add Error page + css

---
+++ src/components/pages/Error.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{ color: "red"}}>
+                Error !
+                You went to a non-existent page
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
